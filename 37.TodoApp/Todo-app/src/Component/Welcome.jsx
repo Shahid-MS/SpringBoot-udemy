b98954f21843fed5568adb21ac755d5e9959dfac
Md Shahid Alam <md.shahidalam.cse24@heritageit.edu.in>
2024-12-04 20:20:16 +0530

    Adding cors and api call using axios

--- 37.TodoApp/Todo-app/src/Component/Welcome.jsx
+++ 37.TodoApp/Todo-app/src/Component/Welcome.jsx
@@ -1,8 +1,47 @@
 import { Link, useParams } from "react-router-dom";
+import {
+  retrieveHelloWorld,
+  retrieveHelloWorldBean,
+  retrieveHelloWorldPathvariable,
+} from "./Api/HelloWorldApiService";
 
 const Welcome = () => {
   //   const params = useParams();
   const { username } = useParams();
+  function callHelloWorld() {
+    retrieveHelloWorld()
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+      .finally(() => {
+        console.log("Finally Clean up");
+      });
+
+    retrieveHelloWorldBean()
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+      .finally(() => {
+        console.log("Finally Clean up");
+      });
+
+    retrieveHelloWorldPathvariable(username)
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+      .finally(() => {
+        console.log("Finally Clean up");
+      });
+  }
 
   return (
     <>
@@ -10,6 +49,11 @@ const Welcome = () => {
         <h1>Welcome {username}</h1>
         <div className="">
           Manage Your <Link to="/todos"> Todos</Link>
+        </div>
+        <div className="m-4">
+          <button className="btn btn-success" onClick={callHelloWorld}>
+            Call Hello World
+          </button>
         </div>
       </div>
     </>
